85d58234b855ffe5d4b22ca6cb96905be6ec2606
Scott Richards <scott.srichards@gmail.com>
2014-10-08 17:25:58 -0700

    Got it working again after breaking it.

--- src/com/BitWyze/polarionlive2014/MainActivity.java
+++ src/com/BitWyze/polarionlive2014/MainActivity.java
@@ -1,19 +1,23 @@
 package com.BitWyze.polarionlive2014;
 
 import android.support.v7.app.ActionBarActivity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.parse.Parse;
 import com.parse.ParseAnalytics;
+import com.BitWyze.polarionlive2014.AgendaQueryAdapter;
 
 public class MainActivity extends ActionBarActivity {
-
+	private AgendaQueryAdapter agendaQueryAdapter;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
     	Parse.initialize(this, "eo7wLLlQekmYfJJ77gmXhQkTfFC3qK0OvKV8pFYK", "r7dROaatklYbtxk9sG8NhXjGJ63M00KnEEWbBaOx");
         super.onCreate(savedInstanceState);
+ //       agendaQueryAdapter = new AgendaQueryAdapter(this);
         setContentView(R.layout.activity_main);
     }
 
@@ -30,8 +34,20 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-       
+        switch (item.getItemId()) {
+
+			case R.id.action_agenda: {
+//				updateAgendaList();
+				break;
+			}
+	
+		}
+        
         return super.onOptionsItemSelected(item);
     }
+    
+//    private void updateAgendaList() {
+//		agendaQueryAdapter.loadObjects();
+//		setListAdapter(agendaQueryAdapter);
+//	}
 }
