450d72a8e7336b79e4bc83c0ffa6f146f4aa0688
Scott Richards <scott.srichards@gmail.com>
2014-10-09 16:21:50 -0700

    Added Enter button for Raffle go back to Main screen.

--- src/com/BitWyze/polarionlive2014/RaffleContactActivity.java
+++ src/com/BitWyze/polarionlive2014/RaffleContactActivity.java
@@ -1,9 +1,13 @@
 package com.BitWyze.polarionlive2014;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class RaffleContactActivity extends Activity {
 	private String testimonial;
@@ -16,6 +20,12 @@ public class RaffleContactActivity extends Activity {
 		if (extras != null) {
 			testimonial = extras.getString("TESTIMONIAL_TEXT");
 		}
+		findViewById(R.id.button1).setOnClickListener(new OnClickListener(){
+			public void onClick(View arg0) {
+				Intent mainActivityIntent = new Intent(getBaseContext(),MainActivity.class);
+				startActivity(mainActivityIntent);
+			}
+		});
 	}
 	
 	 @Override
