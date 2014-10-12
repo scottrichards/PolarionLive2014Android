c91e4efc813d7a8d54b9af7edce5eb85cd88694f
Scott Richards <scott.srichards@gmail.com>
2014-10-12 14:43:54 -0700

    Update Layouts Got Checkbox working.

--- src/com/BitWyze/polarionlive2014/RaffleActivity.java
+++ src/com/BitWyze/polarionlive2014/RaffleActivity.java
@@ -3,6 +3,8 @@ package com.BitWyze.polarionlive2014;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -25,18 +27,45 @@ public class RaffleActivity extends Activity {
 		setContentView(R.layout.raffle_view);
 		findViewById(R.id.button1).setOnClickListener(new OnClickListener(){
 			public void onClick(View arg0) {
-				
-				Intent raffleContactIntent = new Intent(getBaseContext(),RaffleContactActivity.class);
-				TextView textView = (TextView)findViewById(R.id.testimonial_text);
-				String testimonialText = textView.getText().toString();
-				if (testimonialText.length() == 0) {
-					Toast.makeText(RaffleActivity.this, "Please provide a testimonial to enter the raffle",
-							Toast.LENGTH_LONG).show();
-				} else {
-					raffleContactIntent.putExtra("TESTIMONIAL_TEXT",testimonialText);
-					startActivity(raffleContactIntent);
-				}
+				onEnter();
 			}
 		});
 	}
+	
+	private void onEnter()
+	{
+		Intent raffleContactIntent = new Intent(getBaseContext(),RaffleContactActivity.class);
+		TextView textView = (TextView)findViewById(R.id.testimonial_text);
+		String testimonialText = textView.getText().toString();
+		if (testimonialText.length() == 0) {
+			Toast.makeText(RaffleActivity.this, "Please provide a testimonial to enter the raffle",
+					Toast.LENGTH_LONG).show();
+		} else {
+			raffleContactIntent.putExtra("TESTIMONIAL_TEXT",testimonialText);
+			startActivity(raffleContactIntent);
+		}	
+	}
+	
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.raffle_contact_menu, menu);
+        return true;
+    }
+	
+	 @Override
+    public boolean onOptionsItemSelected(MenuItem item) 
+	 {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+			case R.id.action_enter_contact_info: {
+				onEnter();
+				break;
+			}
+		}
+        
+        return super.onOptionsItemSelected(item);
+    }
 }
--- src/com/BitWyze/polarionlive2014/RaffleContactActivity.java
+++ src/com/BitWyze/polarionlive2014/RaffleContactActivity.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -29,38 +30,37 @@ public class RaffleContactActivity extends Activity {
 		}
 		findViewById(R.id.button1).setOnClickListener(new OnClickListener(){
 			public void onClick(View arg0) {
-				Intent mainActivityIntent = new Intent(getBaseContext(),MainActivity.class);
-				ParseObject testimonialObject = new ParseObject("Testimonial");
-				TextView nameView = (TextView)findViewById(R.id.name_text);
-				TextView companyView = (TextView)findViewById(R.id.company_text);
-				TextView titleView = (TextView)findViewById(R.id.title_text);
-				TextView emailView = (TextView)findViewById(R.id.email_text);
-				testimonialObject.put("company", companyView.getText().toString());
-				testimonialObject.put("email", emailView.getText().toString());
-				testimonialObject.put("name", nameView.getText().toString());
-				testimonialObject.put("title", titleView.getText().toString());
-				testimonialObject.put("tesimonial", testimonial);
-				if (companyView.getText().length() == 0 || emailView.getText().length() == 0 || nameView.getText().length() == 0 || titleView.getText().length() == 0) {
-					Toast.makeText(RaffleContactActivity.this, "Please provide all contact information to enter the raffle",
-							Toast.LENGTH_LONG).show();
-				} else {
-					testimonialObject.saveInBackground(new SaveCallback() {
-						@Override
-						public void done(ParseException e) {
-						
-							if (e == null) {
-								Log.d(TAG,"SUCCESS");
-						     } else {
-						    	 Log.d(TAG,"FAILURE"); 
-						     }
-						}
-					});
-					startActivity(mainActivityIntent);
-				}
+				addTestimonial();
+//				Intent mainActivityIntent = new Intent(getBaseContext(),MainActivity.class);
+//				ParseObject testimonialObject = new ParseObject("Testimonial");
+//				TextView nameView = (TextView)findViewById(R.id.name_text);
+//				TextView companyView = (TextView)findViewById(R.id.company_text);
+//				TextView titleView = (TextView)findViewById(R.id.title_text);
+//				TextView emailView = (TextView)findViewById(R.id.email_text);
+//				testimonialObject.put("company", companyView.getText().toString());
+//				testimonialObject.put("email", emailView.getText().toString());
+//				testimonialObject.put("name", nameView.getText().toString());
+//				testimonialObject.put("title", titleView.getText().toString());
+//				testimonialObject.put("tesimonial", testimonial);
+//				if (companyView.getText().length() == 0 || emailView.getText().length() == 0 || nameView.getText().length() == 0 || titleView.getText().length() == 0) {
+//					Toast.makeText(RaffleContactActivity.this, "Please provide all contact information to enter the raffle",
+//							Toast.LENGTH_LONG).show();
+//				} else {
+//					testimonialObject.saveInBackground(new SaveCallback() {
+//						@Override
+//						public void done(ParseException e) {
+//						
+//							if (e == null) {
+//								Log.d(TAG,"SUCCESS");
+//						     } else {
+//						    	 Log.d(TAG,"FAILURE"); 
+//						     }
+//						}
+//					});
+//					startActivity(mainActivityIntent);
+//				}
 			}
 		});
-		
-		
 	}
 	
 	 @Override
@@ -69,9 +69,55 @@ public class RaffleContactActivity extends Activity {
         getMenuInflater().inflate(R.menu.raffle_contact_menu, menu);
         return true;
     }
-	 
 	
-	 
+	 @Override
+    public boolean onOptionsItemSelected(MenuItem item) 
+	 {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+
+			case R.id.action_enter_contact_info: {
+				addTestimonial();
+				break;
+			}
+		}
+        
+        return super.onOptionsItemSelected(item);
+    }
+
+	private void addTestimonial() {
+		Intent mainActivityIntent = new Intent(getBaseContext(),MainActivity.class);
+		ParseObject testimonialObject = new ParseObject("Testimonial");
+		TextView nameView = (TextView)findViewById(R.id.name_text);
+		TextView companyView = (TextView)findViewById(R.id.company_text);
+		TextView titleView = (TextView)findViewById(R.id.title_text);
+		TextView emailView = (TextView)findViewById(R.id.email_text);
+		testimonialObject.put("company", companyView.getText().toString());
+		testimonialObject.put("email", emailView.getText().toString());
+		testimonialObject.put("name", nameView.getText().toString());
+		testimonialObject.put("title", titleView.getText().toString());
+		testimonialObject.put("tesimonial", testimonial);
+		if (companyView.getText().length() == 0 || emailView.getText().length() == 0 || nameView.getText().length() == 0 || titleView.getText().length() == 0) {
+			Toast.makeText(RaffleContactActivity.this, "Please provide all contact information to enter the raffle",
+					Toast.LENGTH_LONG).show();
+		} else {
+			testimonialObject.saveInBackground(new SaveCallback() {
+				@Override
+				public void done(ParseException e) {
+				
+					if (e == null) {
+						Log.d(TAG,"SUCCESS");
+				     } else {
+				    	 Log.d(TAG,"FAILURE"); 
+				     }
+				}
+			});
+			startActivity(mainActivityIntent);
+		}
+	}
+
 	 // Move to the Raffle Contact Info Screen
 	 public void onEnter() {
 	 }
